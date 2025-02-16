86f45fe002d716dd8b8c11a710812f0d05b52a43
John Mayou <john@johnmayou.com>
2025-02-16 16:58:00 -0600

    2

---
+++ chapter-2/2.py
@@ -0,0 +1,2 @@
+# example of application in which adaptability can mean the difference between success and bankruptcy
+# - SEO marketing tools, since the algorithms constantly change (ex: Google Search)
