f6ae45eae271e32140792e6169cdbc84330671a2
John Mayou <john@johnmayou.com>
2025-02-17 16:40:49 -0600

    21

---
+++ chapter-2/21.py
@@ -0,0 +1,3 @@
+# I don't really see any real world efficiency disadvantages of using very shallow
+# inheritance hierarchies. If it's the right way to solve the problem, the trade-offs
+# are likely worth it.
