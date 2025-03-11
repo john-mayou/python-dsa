eb6175119b3193b016b0579ee9707d7f0c759b54
John Mayou <john@johnmayou.com>
2025-03-11 17:05:29 -0500

    8

---
+++ chapter-3/8.py
@@ -0,0 +1,11 @@
+# Ordered by asymptotic growth rate:
+# #  equation            big O
+# 1. 2^10                O(1)
+# 2. 4n                  O(n)
+# 3. 2^log(n)            O(n)
+# 4. 3n+100log(n)        O(n)
+# 5. nlog(n)             O(nlog(n))
+# 6. 4n*log(n)+2n        O(nlog(n))
+# 7. (n^2)+10n           O(n^2)
+# 8. n^3                 O(n^3)
+# 9. 2^n                 O(2^n)
