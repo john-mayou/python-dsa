a12e0306eb2b1ae60adf73ab2dcc46c7dc98ed51
John Mayou <john@johnmayou.com>
2025-03-11 16:11:37 -0500

    2

---
+++ chapter-3/2.py
@@ -0,0 +1,17 @@
+# A: 8n*log(n)
+# B: 2n^2
+#
+# n0 such that A is better than B for n >= n0: 17
+# 
+# 8n*log(n) < 2n^2
+# 4n*log(n) < n^2
+# 4log(n) < n
+#
+# n = 15
+# 15.6 < 15: False
+#
+# n = 16
+# 16 < 16: False
+#
+# n = 17
+# 16.3 < 17: True
