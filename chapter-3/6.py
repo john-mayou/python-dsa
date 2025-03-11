1814bfc449ada2fafc5dc7bade4204648e0d75eb
John Mayou <john@johnmayou.com>
2025-03-11 16:50:45 -0500

    6

---
+++ chapter-3/6.py
@@ -0,0 +1,9 @@
+# What is the sum of all the even numbers from 0 to 2n, for any positive integer n?
+#
+# Arithmetic series summation formula: (n of terms)((first term + last term) / 2)
+#
+# n of terms: n + 1 (since were starting from 0)
+# first term: 0
+# last term: 2n
+#
+# formula: n+1(2n/2) = (n+1)n
