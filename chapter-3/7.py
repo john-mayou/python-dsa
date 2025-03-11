a4988d05951f6663a2beb47d06abee441677c610
John Mayou <john@johnmayou.com>
2025-03-11 16:55:42 -0500

    7

---
+++ chapter-3/7.py
@@ -0,0 +1,3 @@
+# (a): says that for all inputs, the runtime is bounded by O(f(n))
+# (b): says that for the worse case, the runtime is bounded by O(f(n))
+# Both statements are the same, although Big O usually uses the first to describe its performance.
