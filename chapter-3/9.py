63e54dac76cd4504eee32755623fdf3060e0fe4c
John Mayou <john@johnmayou.com>
2025-03-16 12:51:38 -0500

    9

---
+++ chapter-3/9.py
@@ -0,0 +1,10 @@
+# Show d(n) is O(f(n)), then ad(n) is O(f(n)), for any constant a > 0:
+# 
+# d(n) <= f(n)
+# ad(n) <= af(n)
+#
+# by definition:
+#   g(n) is O(f(n)) if g(n) <= cf(n) while c > 0
+#
+# thus:
+# ad(n) is O(f(n)))
