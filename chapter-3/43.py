d261557e157be49a4ce1ef450117856dc59d1fb0
John Mayou <john@johnmayou.com>
2025-03-19 17:59:50 -0500

    43

---
+++ chapter-3/43.py
@@ -0,0 +1,7 @@
+"""
+Draw a visual justification of Proposition 3.3 analogous to that of
+Figure 3.3(b) for the case when n is odd:
+
+when n is odd we can split the "middle" bar, and stack them. This
+will extend the x-axis by 0.5 and the y-axis by n+1.
+"""
