6cff42e9cf86902bab303bf55f54142d245be3a9
John Mayou <john@johnmayou.com>
2025-03-19 01:24:33 -0500

    34

---
+++ chapter-3/34.py
@@ -0,0 +1,18 @@
+"""
+There is a well-known city (which will go nameless here) whose inhabi-
+tants have the reputation of enjoying a meal only if that meal is the best
+they have ever experienced in their life. Otherwise, they hate it. Assum-
+ing meal quality is distributed uniformly across a person's life, describe
+the expected number of times inhabitants of this city are happy with their
+meals?
+
+Harmonic series: f(n) ~= ln(n) + 0.577 (Euler-Mascheroni constant)
+
+3 meals per day
+365 days per year
+80 years
+
+total meals = 3*365*80 = 87600
+
+ln(87600) + 0.577 ~= 12 meals
+"""
