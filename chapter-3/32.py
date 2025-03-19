6df330da6a49e55b4c649ddd89c1fe7567c8ff6d
John Mayou <john@johnmayou.com>
2025-03-19 01:10:32 -0500

    32

---
+++ chapter-3/32.py
@@ -0,0 +1,7 @@
+"""
+Given an n-element sequence S, Algorithm D calls Algorithm E on each
+element S[i]. Algorithm E runs in O(i) time when it is called on element
+S[i]. What is the worst-case running time of Algorithm D?
+
+O(n^2)
+"""
