6f6c99849f26544a25e414e368644d051ae96199
John Mayou <john@johnmayou.com>
2025-03-19 01:08:27 -0500

    31

---
+++ chapter-3/31.py
@@ -0,0 +1,8 @@
+"""
+Given an n-element sequence S of integers, Algorithm C executes an
+O(n)-time computation for each even number in S, and an O(log(n))-time
+computation for each odd number in S. What are the best-case and worst-
+case running times of Algorithm C?
+
+Best: O(log(n)) - Worst: O(n)
+"""
