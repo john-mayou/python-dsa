3f7636f91072b2691259f674c261a3990deaa67b
John Mayou <john@johnmayou.com>
2025-03-19 18:38:11 -0500

    47

---
+++ chapter-3/47.py
@@ -0,0 +1,7 @@
+"""
+Let S be a set of n lines in the plane such that no two are parallel and
+no three meet in the same point. Show, by induction, that the lines in S
+determine (theta)(n2) intersection points.
+
+DNA
+"""
