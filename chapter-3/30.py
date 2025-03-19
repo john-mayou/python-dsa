bd90aba3da5b379366453326cfdb1f581d0d76ca
John Mayou <john@johnmayou.com>
2025-03-19 01:05:23 -0500

    30

---
+++ chapter-3/30.py
@@ -0,0 +1,7 @@
+"""
+Given an n-element sequence S, Algorithm B chooses log(n) elements in
+S at random and executes an O(n)-time calculation for each. What is the
+worst-case running time of Algorithm B?
+
+O(n*log(n))
+"""
