416bc98ec6deef6b7200eda9de24b34747f68f28
John Mayou <john@johnmayou.com>
2025-03-19 00:13:20 -0500

    21

---
+++ chapter-3/21.py
@@ -0,0 +1,10 @@
+"""
+Show that n*log(n) is (omega)(n):
+
+n*log(n) >= n
+log(n) >= 1
+log(n) >= c*1
+
+this inequality holds for c = 1 and n >= 2, thus by definition of Big Omega:
+  n*log(n) is (omega)(n)
+"""
