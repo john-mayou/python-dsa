bcdf88ab214fa60a0f3f0313bb000a81ec946811
John Mayou <john@johnmayou.com>
2025-03-19 00:11:22 -0500

    20

---
+++ chapter-3/20.py
@@ -0,0 +1,10 @@
+"""
+Show that n^2 is (omega)(n*log(n)):
+
+n^2 >= n*log(n)
+n >= log(n)
+n >= c*log(n)
+
+this inequality holds for c = 1 and n >= 2, thus by definition of Big Omega:
+  n^2 is (omega)(n*log(n))
+"""
