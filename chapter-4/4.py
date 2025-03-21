3c3e2f20c2e1d2abbb1543b9fd15670ee0a99d12
John Mayou <john@johnmayou.com>
2025-03-21 16:53:04 -0500

    4 optimized

--- chapter-4/4.py
+++ chapter-4/4.py
@@ -1,3 +1,7 @@
+# reverse([4, 3, 6, 2, 6], 0, 5)
+# reverse([6, 3, 6, 2, 4], 1, 4)
+# reverse([4, 2, 6, 3, 6], 2, 3)
+
 def reverse(S):
     """
     Time -> O(n)
@@ -11,6 +15,18 @@ def reverse(S):
     reverse_helper(0, len(S))
             
     return S
+
+def reverse2(S):
+    """
+    Time -> O(n)
+    Space -> O(1)
+    """
+    left, right = 0, len(S)-1
+    while left < right:
+        S[left], S[right] = S[right], S[left]
+        left += 1
+        right -= 1
+    return S
         
 def test_reverse():
     assert(reverse([]) == [])
@@ -20,7 +36,12 @@ def test_reverse():
     assert(reverse([1, 2, 3]) == [3, 2, 1])
     assert(reverse([3, 2, 1]) == [1, 2, 3])
     assert(reverse([5, 7, 9, 2]) == [2, 9, 7, 5])
-    
-# reverse([4, 3, 6, 2, 6], 0, 5)
-# reverse([6, 3, 6, 2, 4], 1, 4)
-# reverse([4, 2, 6, 3, 6], 2, 3)
+
+def test_reverse2():
+    assert(reverse2([]) == [])
+    assert(reverse2([0]) == [0])
+    assert(reverse2([0, 1]) == [1, 0])
+    assert(reverse2([1, 0]) == [0, 1])
+    assert(reverse2([1, 2, 3]) == [3, 2, 1])
+    assert(reverse2([3, 2, 1]) == [1, 2, 3])
+    assert(reverse2([5, 7, 9, 2]) == [2, 9, 7, 5])
